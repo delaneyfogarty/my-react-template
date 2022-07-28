afc944d2109c470322de312415f191c8ce12ac3f
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-28 16:45:44 -0700

    getTypes fetch function created

--- src/components/state/services/pokedex-service.js
+++ src/components/state/services/pokedex-service.js
@@ -5,6 +5,10 @@ export async function getPokedex(searchParams, page) {
   return await get(`${API_URL}?${searchParams.toString()}&page=${page}`);
 }
 
+export async function getTypes() {
+  return await get(TYPES_URL);
+}
+
 async function get(url) {
   const res = await fetch(url);
   const body = await res.json();
