fe2ff98dc2b509b94752188cbd512a5e4cfd1670
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-28 16:58:57 -0700

    pokedex.jsx set up to fetch pokedex and passprops to pokemonlist

---
+++ src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,18 @@
+import { useSearchParams } from 'react-router-dom';
+import { usePokedex } from '../../state/hooks/pokedex.js';
+import PokemonList from './PokemonList.jsx';
+import Search from './Search.jsx';
+
+export default function Pokedex() {
+  const [searchParams] = useSearchParams();
+  const { pokedex } = usePokedex(searchParams);
+
+  if (!pokedex) return null;
+
+  return (
+    <section>
+      <Search />
+      <PokemonList pokedex={pokedex} />
+    </section>
+  );
+}
---
+++ src/components/state/hooks/pokedex.js
@@ -0,0 +1,4 @@
+// import { useState, useEffect } from 'react';
+// import { getPokedex, getTypes } from '../services/pokedex-service';
+
+// export function usePokedex(searchParams, options) {}
