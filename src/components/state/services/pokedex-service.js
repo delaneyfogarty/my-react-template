03c83c899d39a007b866f288fc6592bfddb491c4
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-28 16:44:54 -0700

    getPokedex fetch function created

--- src/components/state/services/pokedex-service.js
+++ src/components/state/services/pokedex-service.js
@@ -1,6 +1,10 @@
 const API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 const TYPES_URL = `${API_URL}/types`;
 
+export async function getPokedex(searchParams, page) {
+  return await get(`${API_URL}?${searchParams.toString()}&page=${page}`);
+}
+
 async function get(url) {
   const res = await fetch(url);
   const body = await res.json();
