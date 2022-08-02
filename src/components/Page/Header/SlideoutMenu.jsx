818e7ff4194b102beefee0b21fe4f09a2cd43420
Delaney Fogarty <delaney.fk@gmail.com>
2022-08-02 11:38:35 -0700

    updated Header.jsx and slideoutmenu

--- src/components/Page/Header/SlideoutMenu.jsx
+++ src/components/Page/Header/SlideoutMenu.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navigation from './Navigation.jsx';
 import styles from './SlideoutMenu.css';
 import classNames from 'classnames';
 
-export default function Menu() {
+export default function SlideoutMenu({ navigation }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const className = classNames(styles.SlideoutMenu, {
     [styles.Open]: isOpen,
   });
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
     setIsOpen((isOpen) => !isOpen);
   };
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const clickHandler = () => setIsOpen(false);
+    document.addEventListener('click', clickHandler);
+
+    const keyHandler = (e) => {
+      if (e.key !== 'Escape') return;
+      clickHandler();
+    };
+    document.addEventListener('keydown', keyHandler);
+
+    return () => {
+      document.removeEventListener('click', clickHandler);
+      document.removeEventListener('keydown', keyHandler);
+    };
+  }, [isOpen]);
 
   return (
     <button className={className} onClick={handleClick}>
       <div className={styles.MenuContainer}>
-        <Navigation />
+        <Navigation navigation={navigation} />
       </div>
     </button>
   );
