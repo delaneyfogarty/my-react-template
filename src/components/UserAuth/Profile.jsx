fb8b3c661de153f9b18b02f2e867437c0ce040cc
Delaney Fogarty <delaney.fk@gmail.com>
2022-08-04 16:27:12 -0700

    created a profile where user can update their name and avatar

---
+++ src/components/UserAuth/Profile.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { useProfile } from '../../state/hooks/userAuth.js';
+import { useForm } from '../../state/hooks/formData.js';
+import { FormButton, InputControl } from '../Forms/FormControls.jsx';
+
+export default function Profile() {
+  const [, updateProfile] = useProfile();
+  const [profile, handleChange] = useForm();
+  const [preview, setPreview] = useState();
+
+  const handlePreview = (e) => {
+    const target = e.target;
+    const [file] = target.files;
+    setPreview(URL.createObjectURL(file));
+    handleChange({
+      target: {
+        name: target.name,
+        value: file,
+      },
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateProfile(profile);
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h1>User Profile</h1>
+
+        <InputControl
+          label="User Name"
+          name="username"
+          required
+          placeholder="enter a user name"
+          value={profile.username}
+          onChange={handleChange}
+        />
+
+        <InputControl
+          label="Avatar"
+          name="avatar"
+          type="file"
+          required
+          onChange={handlePreview}
+        >
+          {preview && <img src={preview} alt="avatar preview" />}
+        </InputControl>
+
+        <FormButton>Update</FormButton>
+      </form>
+    </section>
+  );
+}
